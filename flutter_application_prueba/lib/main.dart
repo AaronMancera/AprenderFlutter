b6c861938fdef671d8005a00226aeb51f9d316ad
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-21 10:42:16 +0100

    [ Seccion4 ] Creacion de un AppBar

--- flutter_application_prueba/lib/main.dart
+++ flutter_application_prueba/lib/main.dart
@@ -1,16 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MyApp());
+//Exportacion de pantallas creada - Logica de creacion de widget
+import 'package:flutter_application_prueba/screens/home_screen.dart';
+
+void main() {
+  //Warning: Los windget StatelessWidght deben de iniciarse con const
+  runApp( const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return MaterialApp(
-      home: Text('Hola Mundo'),
+    //Warning: En las ultimas versiones de flutter, los datos y widget de estado fijo, es decir; nunca cambian, deben de ser const
+    return const MaterialApp(
+      //Elimina la etique de debug arriba a la izquierda
+      debugShowCheckedModeBanner: false,
+      home: HomeScreen()
     );
   }
-
 }
