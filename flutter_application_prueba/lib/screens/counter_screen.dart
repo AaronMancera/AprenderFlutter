6c6f4968e2145198d48b27c948162ef4bf010695
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-21 12:04:01 +0100

    [ Seccion5 ] Finalizada la aplicacion de prueba de contador. Esta version viene refactorizada y comentada

--- flutter_application_prueba/lib/screens/counter_screen.dart
+++ flutter_application_prueba/lib/screens/counter_screen.dart
@@ -11,6 +11,20 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
   //Para que cambie y atribuir este valor al estado hay que elevarlo en la clase
   int contador = 2;
+  void incrementar() {
+    contador++;
+    setState(() {});
+  }
+
+  void decrementar() {
+    contador--;
+    setState(() {});
+  }
+
+  void resetear() {
+    contador = 0;
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -64,53 +78,50 @@ class _CounterScreenState extends State<CounterScreen> {
       */
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
       floatingActionButton:
-          Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-        FloatingActionButton(
-          child: const Icon(
-            Icons.add_circle_outline,
-          ),
-          onPressed: () {
-            //Debido a que es un stalessWidget hay que utilizar un gestor de estados (StatefulWidget)
-            //contador++;
-            setState(() {
-              //funciona con la suma dentro y fuera
-              contador++;
-            });
-          },
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        FloatingActionButton(
-          child: const Icon(
-            Icons.sync,
-          ),
-          onPressed: () {
-            //Debido a que es un stalessWidget hay que utilizar un gestor de estados (StatefulWidget)
-            //contador++;
-            setState(() {
-              //funciona con la suma dentro y fuera
-              contador = 0;
-            });
-          },
-        ),
-        const SizedBox(
-          width: 20,
-        ),
-        FloatingActionButton(
-          child: const Icon(
-            Icons.remove_circle_outline,
-          ),
-          onPressed: () {
-            //Debido a que es un stalessWidget hay que utilizar un gestor de estados (StatefulWidget)
-            //contador++;
-            setState(() {
-              //funciona con la suma dentro y fuera
-              contador--;
-            });
-          },
-        ),
-      ]),
+          //Mandamos la referencia de la funcion que cambia de estado
+          CustomFloatingActions(
+        incrementarFn: incrementar,
+        decrementarFn: decrementar,
+        resetearFn: resetear,
+      ),
     );
+  }
+}
+
+class CustomFloatingActions extends StatelessWidget {
+  final Function incrementarFn;
+  final Function decrementarFn;
+  final Function resetearFn;
+
+  const CustomFloatingActions({
+    super.key,
+    required this.incrementarFn,
+    required this.decrementarFn,
+    required this.resetearFn,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      FloatingActionButton(
+        child: const Icon(
+          Icons.add_circle_outline,
+        ),
+        //recoge la funciona de incrementar y la ejecuta aqui
+        onPressed: () => incrementarFn(),
+      ),
+      FloatingActionButton(
+        child: const Icon(
+          Icons.sync,
+        ),
+        onPressed: () => resetearFn(),
+      ),
+      FloatingActionButton(
+        child: const Icon(
+          Icons.remove_circle_outline,
+        ),
+        onPressed: () => decrementarFn(),
+      ),
+    ]);
   }
 }
