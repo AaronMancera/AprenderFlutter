78fb96e6bb6106163911e200c9aa91dd91300050
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-22 09:49:12 +0100

    [ Seccion6 ] Aplicando el ListView.separated

--- flutter_application_probar_widget/lib/main.dart
+++ flutter_application_probar_widget/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 //Pantallas
-import 'package:flutter_application_probar_widget/screens/listview1_screen.dart';
+//import 'package:flutter_application_probar_widget/screens/listview1_screen.dart';
+import 'package:flutter_application_probar_widget/screens/listview2_screen%20.dart';
 
 void main() => runApp(const MyApp());
 
@@ -13,7 +14,8 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'Material App',
       //vista personalizada
-      home: Listview1Screen()
+      // home: Listview1Screen(),
+      home: Listview2Screen(),
     );
   }
 }
