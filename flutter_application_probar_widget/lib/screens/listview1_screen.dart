76cae2ba35a38f3f29ceebbb79720afb42fb5fb3
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-22 09:38:47 +0100

    [ Seccion6 ] Creacion de una listaview con datos de una lista

--- flutter_application_probar_widget/lib/screens/listview1_screen.dart
+++ flutter_application_probar_widget/lib/screens/listview1_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Listview1Screen extends StatelessWidget {
+  final options = const['Megaman','Metal Gear', 'Super Smash', 'Final Fantasy'];
   const Listview1Screen({Key? key}) : super(key: key);
 
   @override
@@ -10,11 +11,20 @@ class Listview1Screen extends StatelessWidget {
         title: const Text("Listview Tipo 1"),
       ),
       body:ListView(
+        //Se genera en runtime = no es const
         children: [
-          Text("Hola mundo"),
-          Text("Hola mundo"),
-          Text("Hola mundo"),
-          Text("Hola mundo"),
+          //operador spreed recorre toda la lista
+          ...options.map((game) => 
+            ListTile(
+              title: Text(game),
+              trailing: const Icon(Icons.arrow_circle_right_outlined),
+            ),
+          ).toList(),
+          
+
+          // ListTile(
+          //   title: Text('HolaMundo'),
+          // ),
         ],
       )
     );
