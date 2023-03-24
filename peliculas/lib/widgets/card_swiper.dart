df1d22b9a0760b5b6e7499d528354d0bd1a1a5e1
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-24 11:08:48 +0100

    [ Seccion 7 ] Creacion del panel de visualizacion de las fotos en forma de card, todo ello realizado en card_swiper.dart

--- peliculas/lib/widgets/card_swiper.dart
+++ peliculas/lib/widgets/card_swiper.dart
@@ -1,3 +1,4 @@
+import 'package:card_swiper/card_swiper.dart';
 import 'package:flutter/material.dart';
 
 class CardSwiper extends StatelessWidget {
@@ -5,10 +6,32 @@ class CardSwiper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
     return Container(
       width: double.infinity,
-      height: 500,
-      color: Colors.red,
+      //50% del alto de la pantalla
+      height: size.height * 0.5,
+      // color: Colors.red,
+      child: Swiper(
+        itemCount: 10,
+        layout: SwiperLayout.STACK,
+        itemWidth: size.width * 0.6,
+        itemHeight: size.height * 0.4,
+        //widget renderizado en la tarjeta
+        itemBuilder: (_, index) {
+          return GestureDetector(
+            onTap: () => Navigator.pushNamed(context, '/details',arguments: 'movie-instance' ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: const FadeInImage(
+                  //Pilla el espacio del contenedor y lo completa
+                  fit: BoxFit.cover,
+                  placeholder: AssetImage('assets/images/loading.gif'),
+                  image: NetworkImage('https://via.placeholder.com/300x400')),
+            ),
+          );
+        },
+      ),
     );
   }
 }
