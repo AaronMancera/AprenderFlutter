a5a20286b77ba396ab9cc329e4cedb977677bdf4
AaronMancera <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-24 10:51:07 +0100

    [ Seccion 7 ] Creacion del home y el widget card_swiper.dart

---
+++ peliculas/lib/widgets/card_swiper.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CardSwiper extends StatelessWidget {
+  const CardSwiper({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 500,
+      color: Colors.red,
+    );
+  }
+}
