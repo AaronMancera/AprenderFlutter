471b5246f8f49a5aaa3598c705a91169e35e1ea2
Aarón <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-29 12:03:53 +0000

    [ Seccion 12 ]
    Se ha preparado la app para insertar a continuacion un crud. El login le falta verificar si existen dichos catos para poder inciar a home

--- productos_app/lib/providers/login_form_provider.dart
+++ productos_app/lib/providers/login_form_provider.dart
@@ -1,13 +1,20 @@
 import 'package:flutter/material.dart';
 
 class LoginFormProvider extends ChangeNotifier {
-  
   GlobalKey<FormState> formKey = GlobalKey<FormState>();
   //La modificacion de estos parametros es mediante un set en el onchange de los inputs
   String email = '';
   String password = '';
+  //Metodo que simula que esta cargando
+  bool _isLoading = false;
+  bool get isLoading => _isLoading;
+  set isLoading(bool value) {
+    _isLoading = value;
+    notifyListeners();
+  }
+
   //Esta vinculado con el formulario de login screan que devvolvera true o false depedniedno de las vaidaciones propias incrustadas en el formulario
-  bool isValidForm(){
+  bool isValidForm() {
     print(formKey.currentState?.validate());
     print('$email - $password');
     return formKey.currentState?.validate() ?? false;
