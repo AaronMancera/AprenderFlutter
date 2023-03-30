baa4ff47e8d10327d927ee4e02f5e8492bb439af
Aarón <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-30 07:55:47 +0000

    [ Seccion 12 ]
    Creacion del prediseño de las cartas (imagen y titulo)

--- productos_app/lib/widgets/product_card.dart
+++ productos_app/lib/widgets/product_card.dart
@@ -1,4 +1,3 @@
-
 import 'package:flutter/material.dart';
 
 class ProductCard extends StatelessWidget {
@@ -10,10 +9,59 @@ class ProductCard extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 20),
       child: Container(
-        margin: const EdgeInsets.only(top: 30,bottom: 50),
+        margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+            alignment: Alignment.bottomLeft,
+            children: const [_BackgroundImage(), _ProductDetails()]),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: const [
+            BoxShadow(color: Colors.black, offset: Offset(0, 7), blurRadius: 10)
+          ]);
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 70,
+      color: Colors.indigo,
+      child: Column(children: const [Text('Producto'),]),
+    );
+  }
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: const FadeInImage(
+          placeholder: AssetImage('images/jar-loading.gif'),
+          image: NetworkImage('https://placehold.co/600x400.png'),
+          //Rellena todo el hueco aunque sea la imagen de menor resolucion
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
