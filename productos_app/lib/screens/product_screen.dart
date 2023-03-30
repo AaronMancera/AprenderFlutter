a96abe6d3fbfc941da59356187a085d744c142cf
Aarón <aaron.mancera.cantador.alu@iesjulioverne.es>
2023-03-30 10:05:15 +0000

    [ Seccion 12 ]
    Finalizacion del diseño y algunas funcionalidades que no estan relacionadas con una BD en la pantalla de screen

--- productos_app/lib/screens/product_screen.dart
+++ productos_app/lib/screens/product_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class ProductScreen extends StatelessWidget {
@@ -12,11 +13,97 @@ class ProductScreen extends StatelessWidget {
           children: [
             Stack(
               //TODO: Insertar todos los campos de los productos
-              children: const [ProductImage()],
+              //Parte de arriba de imagen y boton de retoceso
+              children: [
+                const ProductImage(),
+                Positioned(
+                  top: 60,
+                  left: 20,
+                  child: IconButton(
+                    icon: const Icon(Icons.arrow_back,
+                        color: Colors.white, size: 40),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                )
+              ],
+            ),
+            //Formulario del propio producto
+            const _ProductForm(),
+            const SizedBox(
+              height: 100,
             )
           ],
         ),
       ),
     );
   }
+}
+
+// Formulario debajo de la imagen
+class _ProductForm extends StatelessWidget {
+  const _ProductForm({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 10),
+      child: Container(
+        width: double.infinity,
+        decoration: _buildBoxDecoration(),
+        //Padding para los inputs
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: Form(
+              child: Column(
+            children: [
+              const SizedBox(
+                height: 10,
+              ),
+              TextFormField(
+                decoration: InputDecorations.authInputDecorations(
+                    hintText: 'Nombre del producto', labelText: 'Nombre'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecorations.authInputDecorations(
+                    hintText: '150€', labelText: 'Precio'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              SwitchListTile(
+                value: true,
+                onChanged: (value) {
+                  //TODO: Pendiente
+                },
+                title: const Text('Disponible'),
+                activeColor: Colors.indigo,
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+            ],
+          )),
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: const BorderRadius.only(
+              bottomRight: Radius.circular(25),
+              bottomLeft: Radius.circular(25)),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.black.withOpacity(0.05),
+              offset: const Offset(0, 5),
+              blurRadius: 5,
+            )
+          ]);
 }
